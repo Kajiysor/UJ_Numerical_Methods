4965da046c8009f8a60394726fd1c3809658d2f3
Kajiysor <mrbarman55@gmail.com>
2021-10-04 20:23:02 +0200

    added zad2a

---
+++ zestaw1/zad2a.py
@@ -0,0 +1,14 @@
+from math import *
+
+m = 1.0
+c = 300000000.0
+v = 1.0
+
+def E_rel():
+    return(float(m*(c**2)*(1/(sqrt(1-(sqrt(v**2)/sqrt(c**2))))-1)))
+
+def E_nrel():
+    return(float((1/2)*m*(v**2)))
+
+print (E_rel())
+print (E_nrel())
